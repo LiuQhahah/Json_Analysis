f8f2eb24bf531494df6f6da6dbc20199944f66ad
LiuQhahah <1192395260@qq.com>
2017-11-26 11:22:24 +0800

    Main中调用请求，但是请求结果不能写在main中，关于网络请求，结果不能通过返回值得到，数据只能在成功的函数体中显示。

--- app/src/main/java/liuqhahahh/njupt/edu/cn/json_analysis/CommData.java
+++ app/src/main/java/liuqhahahh/njupt/edu/cn/json_analysis/CommData.java
@@ -2,14 +2,35 @@ package liuqhahahh.njupt.edu.cn.json_analysis;
 
 import android.util.Log;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.util.List;
+
+import okhttp3.CacheControl;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  *
  * Created by root on 17-11-21.
  */
 
-public class CommData {
+    public class CommData {
+
+    private static final String TAG = "CommData";
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
+
+
+
+    public CommData() {
+
+    }
+
 
 
     public List<CloudantData> getRows() {
@@ -38,24 +59,8 @@ public class CommData {
         this.offset = offset;
     }
 
-
-
     private String total_rows;
     private String offset;
-
-
-   /* public List<Rows> getRows() {
-        return rows;
-    }
-
-    public void setRows(List<Rows> rows) {
-        this.rows = rows;
-    }
-
-    private List<Rows> rows;*/
-
-
-
 
 
 }
--- app/src/main/java/liuqhahahh/njupt/edu/cn/json_analysis/MainActivity.java
+++ app/src/main/java/liuqhahahh/njupt/edu/cn/json_analysis/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
    private final Gson gson = new Gson();
 
 
-   private final String url = "https://fb2e1a82-8890-4143-810d-e5c79f44a611-bluemix.cloudant.com/nodered/_all_docs?include_docs=true&limit=100";
+   private final String url = "https://fb2e1a82-8890-4143-810d-e5c79f44a611-bluemix.cloudant.com/" +
+           "nodered/_all_docs?include_docs=true&limit=10";
     private String humidity;
     private String temp;
 
@@ -37,57 +38,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getCloudantData();
 
-        /**
-         * 主线程中不给进行网络请求，开启子线程：运行json函数
-         */
-
-       /* new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Request request = new Request.Builder()
-                        .url("https://api.github.com/gists/c2a7c39532239ff261be")
-                        .build();
-                Response response = null;
-                try {
-                    response = client.newCall(request).execute();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                if (!response.isSuccessful())
-                    try {
-                        throw new IOException("Unexpected code "+response);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                // 最为关键！！的方法
-                Gist gist = gson.fromJson(response.body().charStream(),Gist.class);
-                //Log.i(TAG,"response:"+response.body().);
-                for (Map.Entry<String,GistFile> entry:gist.files.entrySet()){
-                    Log.i(TAG,entry.getKey()+":"+entry.getValue().content);
-                }
-            }
-        }).start();
-
-*/
-
-       getCloudantData();
     }
 
-   /* static class Gist{
-        Map<String,GistFile> files;
-    }
-
-    static class GistFile{
-        String content;
-    }*/
 
    private void getCloudantData(){
        Request request   = new Request.Builder()
                .url(url)
-
                .cacheControl(CacheControl.FORCE_NETWORK)
                .build();
 
@@ -99,44 +57,22 @@ public class MainActivity extends AppCompatActivity {
 
            @Override
            public void onResponse(Call call, Response response) throws IOException {
-                Log.i(TAG,"okhttp is request success!");
-                //获取服务器返回的json字符串
+               Log.i(TAG,"okhttp is request success!");
+               //获取服务器返回的json字符串
                String responseString = response.body().string();
-               Log.i(TAG,"获取字符串responseString:"+responseString);
+
                //使用Gson 解析json字符串
                CommData commData  = gson.fromJson(responseString,CommData.class);
 
-
+               // 将"rows"信息将数据传输到CloudantData中，rows有100个数组组成
                List<CloudantData> cloudantDataList = commData.getRows();
 
                Log.i(TAG,"cloudantDataList size:"+cloudantDataList.size());
                for (int i= 0;i<cloudantDataList.size();i++){
                    final String temp = cloudantDataList.get(i).doc.payload.d.getTemp();
                    final String humidity = cloudantDataList.get(i).doc.payload.d.getHumidity();
-                   //在主线程中修改UI
-                   runOnUiThread(new Runnable() {
-                       @Override
-                       public void run() {
-                           //进行图标刷新。。。。
-                           Log.i(TAG,"temp:"+temp+"\n humidity:"+humidity);
-                       }
-                   });
+                   Log.i(TAG,"temp:"+temp+", humidity:"+humidity);
                }
-
-            /*   List<Rows> rows =  commData.getRows();
-               Log.i(TAG,"cloudantDataList size:"+rows.size());
-               for (int i= 0;i<rows.size();i++){
-                    temp =rows.get(i).getDoc().getPayload().getD().getTemp();
-                    humidity = rows.get(i).getDoc().getPayload().getD().getHumidity();
-                   //在主线程中修改UI
-                   runOnUiThread(new Runnable() {
-                       @Override
-                       public void run() {
-                           //进行图标刷新。。。。
-                           Log.i(TAG,"temp:"+temp+"\n humidity:"+humidity);
-                       }
-                   });
-               }*/
            }
        });
    }
